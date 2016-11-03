60943121a282a393c5a0edb7dcef88ecdb6142a2
Anthony Duque <duquea95@gmail.com>
2016-11-03 17:47:12 -0400

    index2 updates

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,28 @@
 $(function(){
     console.log("Loaded");
 
-function testCall(){
-    $.ajax({
-        url: "http://www.nullster.com/ssds/19EA4B39-2221-45CD-A954-2281000AEDBE0EF/ept/modules/server",
-        method: "GET",
-        success: function(data){
-            let results = data;
-            console.log(results);
-        }
-    });
-};
+    function testCall(){
+        $.ajax({
+            url: "http://www.nullster.com/ssds/19EA4B39-2221-45CD-A954-2281000AEDBE0EF/ept/modules/server",
+            method: "GET",
+            success: function(data){
+                let results = data;
+                console.log(results);
+            }
+        });
+    };
 
-testCall();
+    // testCall();
+
+    /* Open the sidenav */
+    /* Set the width of the side navigation to 250px */
+    function openNav(){
+        document.getElementById("mySidenav").style.width = "250px";
+    }
+
+    /* Set the width of the side navigation to 0 */
+    function closeNav(){
+        document.getElementById("mySidenav").style.width = "0";
+    }
 
 });
