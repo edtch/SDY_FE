8c5b6d5772f96a6672ff1873eede0c403d1ba353
Anthony Duque <duquea95@gmail.com>
2016-11-06 13:37:39 -0500

    ajax

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,30 @@
 $(function(){
     console.log("Loaded");
 
+    $("#signUp").on("click", function(){
+        console.log('Clicked');
+        testCall();
+    })
+
     function testCall(){
         $.ajax({
-            url: "http://www.nullster.com/ssds/19EA4B39-2221-45CD-A954-2281000AEDBE0EF/ept/modules/server",
+            url: "http://www.nullster.com/ssds/19EA4B39-2221-45CD-A954-2281000AEDBE0EF/ept/modules/server/1.0.0/user/create/",
+            contentType:'application/json',
             method: "GET",
             success: function(data){
                 let results = data;
-                console.log(results);
+                console.log('hello');
             }
         });
     };
 
-    // testCall();
+    // using XMLHttpRequest
+    // var xhr = new XMLHttpRequest();
+    // xhr.open("GET", "http://www.nullster.com/ssds/19EA4B39-2221-45CD-A954-2281000AEDBE0EF/ept/modules/server/", true);
+    // xhr.onload = function () {
+    //     console.log(xhr.responseText);
+    // };
+    // xhr.send();
 
     /* Open the sidenav */
     /* Set the width of the side navigation to 250px */
