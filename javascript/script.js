5505313207793560d2d89bda45e71bcc62ba0364
System Administrator <root@anthony.fios-router.home>
2016-11-06 13:48:44 -0500

    ajax

--- javascript/script.js
+++ javascript/script.js
@@ -7,14 +7,14 @@ $(function(){
     })
 
     function testCall(){
+        // jQuery preflight request
         $.ajax({
-            url: "http://www.nullster.com/ssds/19EA4B39-2221-45CD-A954-2281000AEDBE0EF/ept/modules/server/1.0.0/user/create/",
-            contentType:'application/json',
-            method: "GET",
-            success: function(data){
-                let results = data;
-                console.log('hello');
-            }
+            type: "GET",
+            headers: {"origin": "http://www.nullster.com/ssds/19EA4B39-2221-45CD-A954-2281000AEDBE0EF/ept/modules/server"},
+            url: "http://www.nullster.com/ssds/19EA4B39-2221-45CD-A954-2281000AEDBE0EF/ept/modules/server/version/user/create"
+        }).done(function (data) {
+            console.log(made it);
+            console.log(data);
         });
     };
 
