cf698014d703d5f6c4d2b314f345a828253c7bbb
System Administrator <root@anthony.fios-router.home>
2016-11-13 14:07:47 -0500

    update

--- javascript/profile.js
+++ javascript/profile.js
@@ -1,14 +1,14 @@
 $(function(){
-    
+
     /* Open the sidenav */
     /* Set the width of the side navigation to 250px */
-    function openNav(){
-        document.getElementById("mySidenav").style.width = "250px";
-    }
-
-    /* Set the width of the side navigation to 0 */
-    function closeNav(){
-        document.getElementById("mySidenav").style.width = "0";
-    }
+    // function openNav(){
+    //     document.getElementById("mySidenav").style.width = "250px";
+    // }
+    //
+    // /* Set the width of the side navigation to 0 */
+    // function closeNav(){
+    //     document.getElementById("mySidenav").style.width = "0";
+    // }
 
 });
