bd3170a512db0fc84604f2c1932061eeedc94bef
System Administrator <root@anthony.local>
2016-12-01 13:10:42 -0500

    stuff

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,6 @@
 $(function(){
 
+
     $("form").on("submit",function(event){
         event.preventDefault();
 
@@ -10,7 +11,7 @@ $(function(){
         var $newAccountPassword = $("input[name=password]").val();
         var $newAccountConfirmPassword = $("input[name=confirmPassword]").val();
         var $newAccountDateOfBirth = $("input[name=dateOfBirth]").val();
-        // console.log($newAccountDateOfBirth);
+        // console.log($("input[name=privacy-checkbox]").val());
         // var newAccountProfilePic = $("input[name=profilePic]").val();
 
         var formData = {
@@ -21,19 +22,18 @@ $(function(){
             "lastName": $newAccountLastName,
             "dateOfBirth": $newAccountDateOfBirth
         };
-        // if ($('#submit') == true){
-        //     console.log(#submit)
-        // }
 
-        validate(formData);
+        if ($("input[name=privacy-checkbox]").is(":checked")){
 
-        if(validate(formData) == true){
-            registrationCall(formData);
+            validate(formData);
+
+        }else{
+            console.log("404 bro!")
         }
 
     });
 
-    var validate = function(formData){
+    var validate = function(formData, response){
         var error = {};
 
         var validate_firstName = /^[A-Za-z\p{L}]{1,36}$/;
@@ -43,60 +43,67 @@ $(function(){
         var validate_dateOfBirth = /^([1-9][0-9][0-9][0-9])-(((01-|03-|05-|07-|08-|10-|12-)(([0-2][0-9])|(3[01])))|((04-|06-|09-|11-)(([0-2][0-9])|(30)))|((02-)(([0-1][0-9])|(2[0-8]))))$/;
         var validate_dateOfBirth2 = /^([1-9][0-9][0-9][0-9])-(((01-|03-|05-|07-|08-|10-|12-)(([0-2][0-9])|(3[01])))|((04-|06-|09-|11-)(([0-2][0-9])|(30)))|((02-)([0-2][0-9])))$/;
 
-        if(validate_firstName.test(formData.firstName)!= true){
-            error.firstName = "Invalid first name!"
-            // $('#errorFirstName').text(error.firstName);
-            $('#errorFirstName').show();
-            console.log(error.firstName);
+        switch (false) {
+            case validate_firstName.test(formData.firstName):
+                error.firstName = "Invalid first name!"
+                // $('#errorFirstName').text(error.firstName);
+                $('#errorFirstName').show();
+                console.log(error.firstName);
+
+            case validate_lastName.test(formData.lastName):
+                error.lastName = "Invalid last name!"
+                // $('#errorLastName').text(error.lastName);
+                $('#errorLastName').show()
+                console.log(error.lastName);
+
+            case validate_email.test(formData.email):
+                error.lastName = "Invalid email!"
+                // $('#errorLastName').text(error.lastName);
+                $('#errorLastName').show()
+                console.log(error.lastName);
+
+            case validate_password.test(formData.password):
+                error.password = "Invalid password!"
+                // $('#errorPassword').text(error.password);
+                $('#errorPassword').show()
+                console.log(error.password);
+
+            case (validate_password.test(formData.confirmPassword) && (formData.confirmPassword == formData.password)):
+                error.confirmPassword = "Password does not match or is invalid!"
+                // $('#errorConfirmPassword').text(error.confirmPassword);
+                $('#errorConfirmPassword').show();
+                console.log(error.confirmPassword);
+
+            case validate_dateOfBirth.test(formData.dateOfBirth):
+                error.dateOfBirth = "Invalid date of Birth!"
+                // $('#errorDateOfBirth').text(error.dateOfBirth);
+                $('#errorDateOfBirth').show();
+                console.log(error.dateOfBirth);
+
+                break
+            default:
+                console.log("Form is valid like salad")
+                $(".errorSpan").hide();
+                // registrationCall(formData);
+
+                break
         }
-        else if(validate_lastName.test(formData.lastName)!= true){
-            error.lastName = "Invalid last name!"
-            // $('#errorLastName').text(error.lastName);
-            $('#errorLastName').show()
-            console.log(error.lastName);
-        }
-        else if(validate_email.test(formData.email)!= true){
-            error.email = "Invalid email!"
-            // $('#errorEmail').text(error.email);
-            $('#errorEmail').show()
-            console.log(error.email);
-        }
-        else if(validate_password.test(formData.password)!= true){
-            error.password = "Invalid password!"
-            // $('#errorPassword').text(error.password);
-            $('#errorPassword').show()
-            console.log(error.length);
-        }
-        else if(formData.confirmPassword != formData.password){
-            error.confirmPassword = "Password does not match or is invalid!"
-            // $('#errorConfirmPassword').text(error.confirmPassword);
-            $('#errorConfirmPassword').show();
-            console.log(error.confirmPassword);
-        }
-        else if(validate_dateOfBirth.test(formData.dateOfBirth)!= true){
-            error.dateOfBirth = "Invalid date of Birth!"
-            // $('#errorDateOfBirth').text(error.dateOfBirth);
-            $('#errorDateOfBirth').show();
-            console.log(error.dateOfBirth);
-        }
-        else{
-            console.log("Form is valid like salad")
-            $(".errorSpan").hide();
-            return true
-        }
+
+
 
     }
 
     var registrationCall = function(formData){
         $.ajax({
-            type: 'POST',
             url: "http://www.nullster.com/ssds/19EA4B39-2221-45CD-A954-2281000AEDBE0EF/ept/redpill/modules/server/user/create",
+            type: 'POST',
             data: formData,
+            dataType: 'json',
             success: function(response){
                 console.log(response)
             },
-            error: function(){
-                console.log(formData);
+            error: function(response){
+                console.log(response);
                 console.log("There was an error submitting comment");
             }
         })
